0b3b538e546598420493afa281d83f59b6e00d9a
Pihtaw <pihtaw@yandex.ru>
2025-07-16 22:36:31 +0200

    yandex draft

--- main.cpp
+++ main.cpp
@@ -8,19 +8,35 @@
 
 using namespace std;
 
-void readFile(vector<Line> &allLines, string filename) {
-  ifstream in(filename);
-  if (in.is_open()) {
-    Time t(0, 0, 0);
-    string line;
-    while (in >> t) {
-      in.ignore();
-      std::string line;
-      std::getline(in, line);
-      allLines.push_back(Line{t, line});
+
+void readFile(std::vector<Line>& allLines, const std::string& filename) {
+    std::ifstream in(filename);
+    if (!in.is_open()) {
+        return;
     }
-  }
-  in.close();
+
+    std::string line;
+    Time t(0, 0, 0);
+    bool hasTime = false;
+    
+    while (std::getline(in, line)) {
+        if (line.empty() || line.find("•") == 0)
+            continue;
+        std::istringstream iss(line);
+        char c1, c2;
+        if (iss >> t.h >> c1 >> t.m >> c2 >> t.s) {
+            if (c1 == ':' && c2 == ':') {
+                size_t endIdx = line.find_first_not_of("0123456789:", 0);
+                if (endIdx != std::string::npos) {
+                    std::string text = line.substr(endIdx);
+                    text.erase(0, text.find_first_not_of(" \t"));
+                    if (!text.empty())
+                        allLines.emplace_back(t, text);
+                }
+            }
+        }
+    }
+    in.close();
 }
 
 void generateNew(vector<Line> &v, vector<Line> &ans, Time x) {
@@ -38,12 +54,13 @@ bool check(vector<Line> &a) {
 
 void writeFile(vector<Line> &a1, vector<Line> &a2, vector<Line> &a3) {
   std::ofstream out("out.txt");
-
+  std::string names[3]{"рутуб", "ВК", "ютуб"};
   bool hasHours;
+  int j = 0;
 
   if (out.is_open()) {
     for (auto i : {a1, a2, a3}) {
-      out << '\n';
+      out << names[j++] << '\n';
       hasHours = check(i);
       for (const Line &timecode : i) {
         if (hasHours) {
@@ -70,7 +87,11 @@ int main() {
 
   Time newTime1(0, 0, 0);
   Time newTime2(0, 0, 0);
-  std::cin >> newTime1 >> newTime2;
+  std::cout << "Enter timings\n";
+  std::cout << "Enter VK start\n";
+  std::cin >> newTime1;
+  std::cout << "Enter Youtube start\n";
+  std::cin >> newTime2;
   generateNew(allLines, vkLines, newTime1);
   generateNew(allLines, youLines, newTime2);
 
